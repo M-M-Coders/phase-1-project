fcc3086a53ff9ff9871bf74080a2743f27fd15fd
clairemuiru1 <144441243+clairemuiru1@users.noreply.github.com>
2023-10-15 16:24:43 +0300

    "commit changes"

--- index.js
+++ index.js
@@ -1,59 +1,64 @@
-// create decor id that is linked to the decor id in the database
-const decorId = getDecorId();
-function getDecorId() {
-  const urlParams = new URLSearchParams(window.location.search);
-  let decorId = urlParams.get("id");
-  if (!decorId) {
-    decorIdd = 1;
+document.addEventListener('DOMContentLoaded', function () {
+  // create decor id that is linked to the decor id in the database
+  const decorId = getDecorId();
+
+  function getDecorId() {
+    const urlParams = new URLSearchParams(window.location.search);
+    let decorId = urlParams.get("id");
+    if (!decorId) {
+      decorId = 1;
+    }
+    return decorId;
   }
-  return decorId;
-}
 
-// Function to update movie details
-//  fetch('http://localhost:3000/decors')
-//  .then(response => response.json())
-//  .then((decor) =>{
-//     const decorDetails = document.getElementById('details');
-//     h1=document.getElementById('name').innerHTML=decor.name;
-//     price=document.getElementById('price').innerHTML=decor.price;
-//     description=document.getElementById('description').innerText=decor.description
-//     color =document.getElementById('color').innerHTML=decor.color;
-//     material =document.getElementById('material').innerHTML=decor.material
-//     height =document.getElementById('height').innerHTML=decor.height
-//     width = document.getElementById('')
-//  })
+  // fetch data from db.json
+  function fetchDecorPieces() {
+    fetch("https://decorniceway.onrender.com/decors")
+      .then((response) => response.json())
+      .then((decors) => {
+        decors.forEach((decor) => {
+          const decorContainer = document.createElement("div");
+          decorContainer.classList.add("decor-piece");
+          decorContainer.innerHTML = `
+            <h2>${decor.name}</h2>
+            <img src="${decor.image}" alt="${decor.name}">
+            <p class="hd">Price (dollars): ${decor.price}</p>
+            <p class="hd">Description: ${decor.description}</p>
+            <p class="hd">Color: ${decor.color}</p>
+            <p class="hd">Material: ${decor.material}</p>
+            <p class="hd">Reviews:${decor.reviews}</p>
+            <p class="hd">Height : ${decor.dimensions.height}</p>
+            <p class="hd">Width: ${decor.dimensions.width}</p>
+            <p class="hd">Diameter: ${decor.dimensions.diameter}</p>
+            <p class="hd">Available-pieces: ${decor.in_stock}</p>
+            <button id="btn">Add to Cart</button>
+            <form id="buy-form">
+              <input id="buy" placeholder="Enter Card Number" name="buy" />
+              <input type="submit" value="Buy Now" />
+            </form>
+          `;
 
-function fetchDecorPieces() {
-  fetch("https://decorniceway.onrender.com/decors")
-    .then((response) => response.json())
-    .then((decors) => {
-      decors.forEach((decor) => {
-        const decorContainer = document.createElement("div");
-        decorContainer.classList.add("decor-piece");
+          document.getElementById("container").appendChild(decorContainer);
 
-        decorContainer.innerHTML = `
-          <h1>${decor.name}</h1>
-          <img src="${decor.image}" alt="${decor.name}">
-          <p class="hd">Price (dollars): ${decor.price}</p>
-          <p class="hd">Description: ${decor.description}</p>
-          <p class="hd">Color: ${decor.color}</p>
-          <p class="hd">Material: ${decor.material}</p>
-          <p class="hd">Height : ${decor.dimensions.height}</p>
-          <p class="hd">Width: ${decor.dimensions.width}</p>
-          <p class="hd">Diametre: ${decor.dimensions.diameter}</p>
-          <p class="hd">Available-pieces: ${decor.in_stock}</p>
-          <button id="btn">Add to Cart</button>
-          <form id="buy-form">
-            <input id="buy" placeholder="Enter Card Number" name="buy" />
-            <input type="submit" value="Buy Now" />
-          </form>
-        `;
+          // Add event listeners
+          const addButton = decorContainer.querySelector('#btn');
+          addButton.addEventListener('click', function () {
+            addButton.innerHTML = "Added To Cart";
+          });
 
-        document.getElementById("container").appendChild(decorContainer);
+          // Add submit event listener to the form within this loop
+          const buyForm = decorContainer.querySelector('#buy-form');
+          buyForm.addEventListener('submit', function (event) {
+            event.preventDefault();
+            const cardNumber = decorContainer.querySelector('#buy').value;
+            console.log('Card number submitted:', cardNumber);
+          });
+        });
+      })
+      .catch((error) => {
+        console.error("Error fetching decor pieces:", error);
       });
-    })
-    .catch((error) => {
-      console.error("Error fetching decor pieces:", error);
-    });
-}
-fetchDecorPieces();
+  }
+
+  fetchDecorPieces();
+});
